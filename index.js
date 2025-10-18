b28fef38379e491036a91bf124830d2fde85e877
Aman Sachaan <amansachan449@gmail.com>
2025-10-18 15:54:21 +0530

    Fix

--- index.js
+++ index.js
@@ -7,29 +7,44 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-// Serve the frontend HTML
+// Serve static files (index.html, socket.io client, etc.)
+app.use(express.static(__dirname));
+
+// Serve the main HTML
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Simple peer pairing logic
+let waitingSocket = null;
+
 io.on('connection', socket => {
   console.log('User connected:', socket.id);
 
-  socket.on('offer', data => {
-    socket.broadcast.emit('offer', data);
-  });
-
-  socket.on('answer', data => {
-    socket.broadcast.emit('answer', data);
-  });
-
-  socket.on('ice-candidate', data => {
-    socket.broadcast.emit('ice-candidate', data);
-  });
+  if (waitingSocket) {
+    // Pair with waiting user
+    socket.on('offer', data => {
+      waitingSocket.emit('offer', data);
+    });
+    socket.on('answer', data => {
+      waitingSocket.emit('answer', data);
+    });
+    socket.on('ice-candidate', data => {
+      waitingSocket.emit('ice-candidate', data);
+    });
+    waitingSocket = null;
+  } else {
+    waitingSocket = socket;
+  }
 
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
+    if (waitingSocket === socket) {
+      waitingSocket = null;
+    }
   });
 });
+
+// Use dynamic port for Render
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
