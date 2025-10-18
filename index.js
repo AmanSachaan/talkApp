6b02ad77da31d83894aa85e22c46f4dcc6595e80
Aman Sachaan <amansachan449@gmail.com>
2025-10-18 15:47:45 +0530

    Fix

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ io.on('connection', socket => {
     console.log('User disconnected:', socket.id);
   });
 });
-
-server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
